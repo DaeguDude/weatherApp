574b6a95fe1b1a8e58078d146cda0532efa06309
Daegudude <k3hppk@gmail.com>
2021-01-22 15:37:08 +0900

    second commit

---
+++ weather.js
@@ -0,0 +1,27 @@
+const apiKey = "2099fd7bf963f2aab2763983636f5bd5";
+const cityName = "New York";
+const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+// Promise
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json(); // returns promise
+//   })
+//   .then((data) => {
+//     console.log("Success:", data);
+//   })
+//   .catch((err) => {
+//     console.error("Error:", error);
+//   });
+
+// How will I do it in async / await
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  console.log(response);
+}
+
+getData();
